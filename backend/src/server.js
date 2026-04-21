e2176c2c9bda4385324b5d699727a91270850d35
Kishan192021 <kishan.m@vmstechs.com>
2026-04-21 22:04:38 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,7 @@
 // const { PORT } = require("./config");
 const { PORT } = require("./config");
 const express = require("express");
-const cors = require("cors");
-
+const cors = require("cors")
 // const port = parseInt(PORT) || 3000;
 
 const app = express();
